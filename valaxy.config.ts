c95fa65f8b87447ddf2f8bd55b2dc6e21c61d6cf
free7iu <21067110214@stu.nsu.edu.cn>
2023-03-28 22:34:02 +0800

    valaxy-config

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -17,7 +17,7 @@ export default defineValaxyConfig<UserThemeConfig>({
   themeConfig: {
     banner: {
       enable: true,
-      title: '云游君的小站',
+      title: '人去影故，落叶听秋。',
       cloud: {
         enable: true,
       },
@@ -39,10 +39,10 @@ export default defineValaxyConfig<UserThemeConfig>({
     ],
 
     footer: {
-      since: 2016,
+      since: 2021,
       beian: {
         enable: true,
-        icp: '苏ICP备17038157号',
+        icp: '萌ICP备1101011号',
       },
     },
   },
